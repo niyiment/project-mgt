6fa0a9f5ac828f9f1a287b7ee4608298679ccc1f
Niyi Ogungbemi <niyiment@gmail.com>
2025-03-15 15:03:49 +0100

    First commit

---
+++ src/test/java/niyiment/projectmgt/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package niyiment.projectmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
