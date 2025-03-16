1c06466bcffad536531e82ddec3d904fe4544740
Niyi Ogungbemi <niyiment@gmail.com>
2025-03-16 04:51:55 +0100

    Add CSV report generator implementation

--- src/main/java/niyiment/projectmgt/report/dto/ReportGenerator.java
+++
@@ -1,9 +0,0 @@
-package niyiment.projectmgt.report.dto;
-
-import reactor.core.publisher.Mono;
-
-public interface ReportGenerator {
-    Mono<Byte> generateReport(ReportData reportData);
-    String getContentType();
-    String getFileExtension();
-}
